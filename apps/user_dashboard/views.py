e7df3f500331734fd7e5b04d4110fed8a9830d8f
jacobsaw <adam.w.jacobs@gmail.com>
2016-08-18 16:51:08 -0400

    add python dependencies

--- apps/user_dashboard/views.py
+++ apps/user_dashboard/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse, redirect
+from django.core.urlresolvers import reverse
+#Count used for aggregate counts - .count also an option when just counting something within one table
+from django.db.models import Count
+# CONTROLLER!!!
 # Create your views here.
+def index(request):
+	#using loginandreg instead of belt_review allows me to pull in html files from other app
+	return render(request, 'user_dashboard/index.html')
